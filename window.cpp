e614e2ce48cee11479830fb992bbd7f7023a864a
Carlos Sanchez <62035084+Sancal0606@users.noreply.github.com>
2026-01-09 23:02:57 -0600

    create window

---
+++ window.cpp
@@ -0,0 +1,55 @@
+#include <windows.h>
+
+//Window procedure (handles messages)
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+    switch(uMsg){
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+//Entry point for Windows apps (not main)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow){
+    // Window class setup
+    const wchar_t CLASS_NAME[] = L"MyWindowClass";
+
+    WNDCLASSW wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+    RegisterClassW(&wc);
+
+    //Create window
+    HWND hwnd = CreateWindowExW(
+        0,
+        CLASS_NAME,
+        L"My Window",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        800, 600,
+        nullptr,
+        nullptr,
+        hInstance,
+        nullptr
+    );
+    if(hwnd == nullptr){
+        return 0;
+    }
+
+    //Show window
+    ShowWindow(hwnd, nCmdShow);
+
+    //Message loop
+    MSG msg = {};
+    while(GetMessage(&msg, nullptr, 0,0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
+}
